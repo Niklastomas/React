b4330b917aa197901ffb69737785ace5b6933c4a
Niklas Tomas <niklas.thomas@hotmail.com>
2020-08-08 14:27:26 +0200

    Second commit

--- login-app/src/components/App.jsx
+++ login-app/src/components/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Login from "./Login";
 
-var isLoggedIn = true;
+const isLoggedIn = false;
+const userIsRegistered = false;
 
 function App() {
   return (
     <div className="container">
       
-      {isLoggedIn ? <h1>Hello</h1> : <Login /> }
+      {isLoggedIn ? <h1>Hello</h1> : <Login isRegistered = {userIsRegistered} /> }
       
     </div>
   );
--- login-app/src/components/Login.jsx
+++ login-app/src/components/Login.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Input from "./Input";
 
-function Login(){
+function Login(props){
 
     return (
         <form className="form">
             <Input placeholder="Username" type="text" />
             <Input placeholder="Password" type="password" />
-        <button type="submit">Login</button>
+            {!props.isRegistered && <Input placeholder="Confim Password" type password /> }   
+        <button type="submit">{props.isRegistered ? "Login" : "Register"}</button>
       </form>
     );
 }
